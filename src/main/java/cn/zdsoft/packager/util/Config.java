5f1e393b105d0bc7d4fd11ee15c04134ede0909f
duanjt1314 <479224006@qq.com>
2017-07-14 17:17:05 +0800

    完成配置文件的读取功能

--- src/main/java/cn/zdsoft/packager/util/Config.java
+++ src/main/java/cn/zdsoft/packager/util/Config.java
@@ -8,7 +8,6 @@ import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-import cn.zdsoft.common.ConvertUtil;
 import cn.zdsoft.common.PathUtil;
 import cn.zdsoft.common.XmlUtil;
 import cn.zdsoft.packager.config.Delete;
